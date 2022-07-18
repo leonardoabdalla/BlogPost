f04789c9611578bd802eb3367ffac5d75e63bb44
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-07-18 14:28:24 -0300

    req 03

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -2,25 +2,25 @@ const Joi = require('joi');
 const db = require('../database/models');
 
 const userServices = {
-    create: async ({ displayName, email, password, image }) => {
-        validateBody: (data) => {
-            const schema = Joi.object({
-                displayName: Joi.string().min(8).require(),
-                email: Joi.string().email().require(),
-                password: Joi.string().min(6).require(),
-            });
-    
-            const { error, value } = schema.validate(data);
-            console.log(error);
-    
-            if (error) {
-               const e = new Error('Some required fields are missing');
-               e.name = 'ValidationError';
-               throw e;
-            }
-            return value;
-        },
+    validateBody: (data) => {
+        const schema = Joi.object({
+            displayName: Joi.string().min(8).require(),
+            email: Joi.string().email().require(),
+            password: Joi.string().min(6).require(),
+        });
         
+        const { error, value } = schema.validate(data);
+        console.log(error);
+        
+        if (error) {
+            const e = new Error('Some required fields are missing');
+            e.name = 'ValidationError';
+            throw e;
+        }
+        return value;
+    },
+    
+    create: async ({ displayName, email, password, image }) => {
         const user = await db.User.create({ displayName, email, password, image });
         return user;
     },
